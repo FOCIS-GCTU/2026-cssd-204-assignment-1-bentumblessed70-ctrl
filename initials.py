cc71dc60f0340f91646de9a47ee96cef1f33289b
bentumblessed70-ctrl <bentumblessed70@gmail.com>
2026-05-31 18:22:58 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
 
+
 def main():
-  pass #code goes here
+    #
+    print("")
+
+#File: initials.py
+#Description: A code displaying the initials of all my first name
+#Assignment Number: Assignment Number 2
+#
+#Name: Bentum Blessed Kwabena Acquah
+#Student ID: 2425404025
+#Email: bentumblessed70@gmail.com
+#Grader: Agustus
+#
+#On my honor,Bentum Blessed Kwabena Acquah, this programming assignment is my own work
+#and I have not provided the work to any other student.
 
 
+print("...BBKA")
+print()
 
+print("...BBBBBBBBBB.........BBBBBBBBBB.........KK      KK........AAAAAAAAAA.....")
+print("...BB       BB........BB       BB........KK    KK..........AA      AA.....")
+print("...BB       BB........BB       BB........KK  KK............AA      AA.....")
+print("...BB       BB........BB       BB........KKKK..............AA      AA.....")
+print("...BBBBBBBBBB.........BBBBBBBBBB.........KKKK..............AAAAAAAAAA.....")
+print("...BB       BB........BB       BB........KKKK..............AA      AA.....")
+print("...BB       BB........BB       BB........KK  KK............AA      AA.....")
+print("...BB       BB........BB       BB........KK    KK..........AA      AA.....")
+print("...BB       BB...**...BB       BB...**...KK      KK...**...AA      AA...**")
+print("...BBBBBBBBBB....**...BBBBBBBBBB....**...KK      KK...**...AA      AA...**")
+
+print()
 main()
